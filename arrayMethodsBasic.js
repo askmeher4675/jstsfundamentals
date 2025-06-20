0d0619d72b4a4341f22cd6333885b823b8c8ae4a
Atulya <askmeher4675@gmail.com>
2025-06-20 23:24:34 +0530

    added arrayMethods and Renamed Files

---
+++ arrayMethodsBasic.js
@@ -0,0 +1,61 @@
+//push() -  adding elements at end of an array
+let arr1 = [1, 2];
+arr1.push(3);
+console.log("Push:", arr1); // [1, 2, 3]
+
+//pop() - removes last element form the array
+arr1.pop();
+console.log("Pop:", arr1); // [1, 2]
+
+//shift() - removes first element from left side
+arr1.shift();
+console.log("Shift:", arr1);//[2]
+
+//unshift() - adds element to the initial array
+arr1.unshift(4);
+console.log("Shift:", arr1);//[4,2]
+
+
+
+
+//slice() - 
+
+//concat() - to combine two arrays
+//explore option to merge two arrays where a similar element is there
+// maintains the indexes 
+
+//indexOf() -
+//not found , returns -1
+//return the index of first instance of occurence
+
+//includes() - whether a certain value is there or not
+//returns true or false
+
+
+//forEach()
+
+//map
+
+//filter
+
+//reduce
+
+//find
+
+//reverse() - reverse the elements
+
+//sort() - sorts the elements and returns the sorted array
+//numbers 
+//alphabets
+//combination of data types
+// 100,20,30
+//100,banana,20,30,apple
+
+
+
+
+
+
+
+
+
