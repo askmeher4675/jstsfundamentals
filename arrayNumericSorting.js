45bff87a00c06182aa78a61210ac291288623946
Atulya <askmeher4675@gmail.com>
2025-06-20 22:57:02 +0530

    added array code

---
+++ arrayNumericSorting.js
@@ -0,0 +1,3 @@
+let arrNum = [100,20,30];
+let arrNumSorted = arrNum.sort((a,b)=> a-b);
+console.log(arrNumSorted); // output : [ 20, 30, 100 ]
---
+++ arraySorting.js
@@ -0,0 +1,33 @@
+// .sort() without a compare function
+//  uses dictionary (lexicographical) order, just like how words are sorted in a dictionary.
+// Breakdown:
+
+// Compare "100" vs "20"
+// → first characters: '1' (ASCII 49) vs '2' (ASCII 50)
+// → '1' < '2' → so "100" comes before "20"
+// Then "30" is compared to the rest similarly.
+
+let arr = [100,20,30];
+let arrSorted = arr.sort();
+console.log(arrSorted); // Output : [ 100, 20, 30 ]
+
+
+/****************
+ * NUMERIC SORTING
+ *****************/
+let arrNum = [100,20,30];
+let arrNumSorted = arrNum.sort((a,b)=> a-b);
+console.log(arrNumSorted); // output : [ 20, 30, 100 ]
+
+/************EOF Numeric Sorting */
+
+let arrMixed = [100,"banana",20,30,"apple"];
+let arrMixedSorted = arrMixed.sort();
+console.log(arrMixedSorted); // Output : [ 100, 20, 30, 'apple', 'banana' ]
+
+
+let arrMixed2 = [100,"banana",20,30,"apple",true];
+
+let arrMixed2Sorted = arrMixed2.sort();
+
+console.log(arrMixed2Sorted); // Output : [ 100, 20, 30, 'apple', 'banana', true ]
